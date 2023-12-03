7a2944c8a88e4053ca280151e4272ce5b5418924
HuHongMing86 <hhm980806@163.com>
2023-12-03 22:02:03 +0800

    添加项目文件。

---
+++ ThreadPool/main.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("%s 向你问好!\n", "ThreadPool");
+    return 0;
+}
---
+++ ThreadPool/threadpool.h
@@ -0,0 +1,11 @@
+#pragma once
+//主要是添加函数的声明
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+
+	return 0;
+}
